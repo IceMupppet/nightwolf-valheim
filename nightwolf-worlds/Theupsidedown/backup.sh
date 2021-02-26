6ea7c89f0052f9de1f4f56fa325120f791c1c81a
IceMupppet <icemuppet+github@gmail.com>
2021-02-26 15:18:30 +0000

    Adding Nightwolf Worlds

---
+++ nightwolf-worlds/Theupsidedown/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Nightwolf Valheim - BACKUP IN PROGRESS....  "
+cp -f /home/steam/.config/unity3d/IronGate/Valheim/worlds/*.* .
+
+
---
+++ nightwolf-worlds/Theupsidedown/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Nightwolf Valheim - REINSTALLATION IN PROGRESS....  "
+cp -f *.db /home/steam/.config/unity3d/IronGate/Valheim/worlds/
+cp -f *.fwl /home/steam/.config/unity3d/IronGate/Valheim/worlds/
+cp -f *.old /home/steam/.config/unity3d/IronGate/Valheim/worlds/
+
+echo "FINSIHED.  -Nightwolf"
+
+
+
